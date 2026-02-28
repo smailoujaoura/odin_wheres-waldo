70a087150e2e4915495b68d8db855e3fb010be30
Smail Oujaoura <email.oujaoura@gmail.com>
2026-02-28 23:37:17 +0000

    remove extra slashes on axios base url

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const base = import.meta.env.VITE_API_URL || "http://localhost:3000"
+const base = (import.meta.env.VITE_API_URL || "http://localhost:3000").replace(/\/+$/, "");
 
 const api = axios.create({
-  baseURL: `${base}/api`,
+	baseURL: `${base}/api`,
 });
 
 export default api;
