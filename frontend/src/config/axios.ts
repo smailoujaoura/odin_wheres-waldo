ca464c1772c7b36d2e66a94a1c4a88a55ca7e5f9
Smail Oujaoura <email.oujaoura@gmail.com>
2026-02-28 23:34:14 +0000

    fix url not having /api on production

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const base = import.meta.env.VITE_API_URL || "http://localhost:3000"
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  baseURL: `${base}/api`,
 });
 
 export default api;
